2b0a0f7d5b16ca8a07edc19bd7ddf3a80524ef07
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-09 13:52:59 -0700

    added post request to /accounts. added validateAccount middleware

--- routes/accountsRouter.js
+++ routes/accountsRouter.js
@@ -16,6 +16,8 @@ router.get('/', (req, res) => {
     })
 })
 
+// GET /accounts/id
+
 router.get('/:id', validateAccountId, (req, res) => {
     const { id } = req.params;
     db('accounts').where('id', id).first().select()
@@ -28,6 +30,19 @@ router.get('/:id', validateAccountId, (req, res) => {
 })
 
 
+// POST /accounts
+
+router.post('/', validateAccount, (req, res) => {
+    const account = req.body;
+
+    db('accounts').insert(account)
+    .then(added => {
+        res.status(201).json(added[0]);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
 
 function validateAccountId (req, res, next) {
     const accountBody = req.body;
@@ -42,6 +57,19 @@ function validateAccountId (req, res, next) {
             res.status(404).json({ message: 'An account with that ID could not be found.' });
         }
     })
+}
+
+function validateAccount (req, res, next) {
+    const accountBody = req.body;
+
+    if (!Object.keys(accountBody).length) {
+        res.status(400).json({ message: 'Account fields cannot be empty.' })
+    } else if (!accountBody.name || !accountBody.budget) {
+        res.status(400).json({ message: 'Name and budget fields cannot be empty.' })
+    } else {
+        next();
+    }
+
 }
 
 
