228dc70a23bbb72ff6cd58d5764b6b285dd514e0
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-09 13:01:00 -0700

    added get request to /api/accounts

--- server.js
+++ server.js
@@ -6,4 +6,15 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/accounts', (req, res) => {
+    db('accounts').select()
+    .then(accounts => {
+        res.status(200).json(accounts)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+
 module.exports = server;
