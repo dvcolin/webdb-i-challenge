ce3b71208d829452b3e2f7860e7c94300e543c39
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-09 13:43:17 -0700

    added get request to /accounts/id. added validateAccountId middleware

--- routes/accountsRouter.js
+++ routes/accountsRouter.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const db = require('../data/dbConfig.js');
 
+// GET /accounts
 
 router.get('/', (req, res) => {
     db('accounts').select()
@@ -14,6 +15,36 @@ router.get('/', (req, res) => {
         res.status(500).json(err)
     })
 })
+
+router.get('/:id', validateAccountId, (req, res) => {
+    const { id } = req.params;
+    db('accounts').where('id', id).first().select()
+    .then(account => {
+        res.status(200).json(account)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+
+
+function validateAccountId (req, res, next) {
+    const accountBody = req.body;
+    const { id } = req.params;
+
+    db('accounts').where('id', id).first().select()
+    .then(response => {
+        if (response) {
+            req.account = accountBody;
+            next();
+        } else {
+            res.status(404).json({ message: 'An account with that ID could not be found.' });
+        }
+    })
+}
+
+
 
 
 module.exports = router;
